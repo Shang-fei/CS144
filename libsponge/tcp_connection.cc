1e6df2dc3ea4a4655b005504505c71d914f07005
“aaa” <aa@qq.com>
2024-01-26 16:07:09 +0800

    finish tcp_connection

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -12,29 +12,129 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-size_t TCPConnection::remaining_outbound_capacity() const { return {}; }
+size_t TCPConnection::remaining_outbound_capacity() const { return _sender.stream_in().remaining_capacity(); }
 
-size_t TCPConnection::bytes_in_flight() const { return {}; }
+size_t TCPConnection::bytes_in_flight() const { return _sender.bytes_in_flight(); }
 
-size_t TCPConnection::unassembled_bytes() const { return {}; }
+size_t TCPConnection::unassembled_bytes() const { return _receiver.unassembled_bytes(); }
 
-size_t TCPConnection::time_since_last_segment_received() const { return {}; }
+size_t TCPConnection::time_since_last_segment_received() const { return _time_since_last_segment_received;}
 
-void TCPConnection::segment_received(const TCPSegment &seg) { DUMMY_CODE(seg); }
+void TCPConnection::segment_received(const TCPSegment &seg) {
+    if(!active())
+        return;
+    
+    _time_since_last_segment_received = 0;
 
-bool TCPConnection::active() const { return {}; }
+    if(seg.header().rst){
+        unclean_shutdown();
+        return;
+    }
+
+    _receiver.segment_received(seg);
+
+    if(TCPState::state_summary(_receiver) == TCPReceiverStateSummary::SYN_RECV &&
+       TCPState::state_summary(_sender) == TCPSenderStateSummary::CLOSED){
+        connect();
+        return;
+       }
+    
+    if(seg.header().ack)
+        _sender.ack_received(seg.header().ackno, seg.header().win);
+    
+    if(seg.length_in_sequence_space() > 0 && _sender.segments_out().empty()){
+        _sender.send_empty_segment();
+    }
+
+    if (_receiver.ackno().has_value() && 
+        seg.length_in_sequence_space() == 0 &&
+        seg.header().seqno == _receiver.ackno().value() - 1) {
+        _sender.send_empty_segment();
+    }
+
+    send_segments();
+    clean_shutdown();
+}
+
+bool TCPConnection::active() const { return _is_active; }
 
 size_t TCPConnection::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    if(data.empty() || !active())
+        return 0;
+
+    size_t n = _sender.stream_in().write(data);
+    _sender.fill_window();
+    send_segments();
+    return n;
 }
 
 //! \param[in] ms_since_last_tick number of milliseconds since the last call to this method
-void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPConnection::tick(const size_t ms_since_last_tick) { 
+    if(!active())
+        return;
 
-void TCPConnection::end_input_stream() {}
+    _sender.tick(ms_since_last_tick);
+    _time_since_last_segment_received += ms_since_last_tick;
 
-void TCPConnection::connect() {}
+    if(_sender.consecutive_retransmissions() > TCPConfig::MAX_RETX_ATTEMPTS){
+        send_rst_segment();
+        unclean_shutdown();
+        return;
+    }
+    send_segments();
+    clean_shutdown();
+}
+
+void TCPConnection::end_input_stream() {
+    _sender.stream_in().end_input();
+    _sender.fill_window();
+    send_segments();
+}
+
+void TCPConnection::connect() {
+    _sender.fill_window();
+    send_segments();
+    return;
+}
+
+void TCPConnection::send_segments() {
+    while(!_sender.segments_out().empty()){
+        TCPSegment seg = _sender.segments_out().front();
+        _sender.segments_out().pop();
+        if(_receiver.ackno().has_value()){
+            seg.header().ack = true;
+            seg.header().ackno = _receiver.ackno().value();
+            seg.header().win = _receiver.window_size() <= numeric_limits<uint16_t>::max()
+                             ? _receiver.window_size()
+                             : numeric_limits<uint16_t>::max();
+        }
+        _segments_out.push(seg);
+    }
+}
+
+void TCPConnection::send_rst_segment() {
+    TCPSegment seg;
+    seg.header().rst = true;
+    segments_out().push(seg);    
+}
+
+void TCPConnection::clean_shutdown() {
+    if(_receiver.stream_out().input_ended() && !_sender.stream_in().eof()) {
+        _linger_after_streams_finish = false;
+    }
+
+    else if(TCPState::state_summary(_receiver) == TCPReceiverStateSummary::FIN_RECV &&
+       TCPState::state_summary(_sender) == TCPSenderStateSummary::FIN_ACKED){
+        if(!_linger_after_streams_finish || _time_since_last_segment_received >= 10 * _cfg.rt_timeout)
+            _is_active = false;
+    } 
+}
+
+void TCPConnection::unclean_shutdown() {
+    _sender.stream_in().set_error();
+    _receiver.stream_out().set_error();
+    _is_active = false;
+}
 
 TCPConnection::~TCPConnection() {
     try {
